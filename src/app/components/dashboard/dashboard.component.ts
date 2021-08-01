34471359fbba6fad0e29114ac1c56f14111542df
Santiago Vivas <60908806+sagov8@users.noreply.github.com>
2021-08-01 02:19:21 -0500

    app complete

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ClimaService } from 'src/app/services/clima.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,12 +9,42 @@ import { Component, OnInit } from '@angular/core';
 export class DashboardComponent implements OnInit {
   urlImagen = 'https://image.flaticon.com/icons/png/512/1116/1116453.png';
   ciudad = '';
+  tempetura = 0;
+  humedad = 0;
+  clima = '';
+  query = false;
+  loading = false;
+  mostrarError = false;
 
-  constructor() {}
+  constructor(private _climaService: ClimaService) {}
 
   ngOnInit(): void {}
 
-  obtenerClima(){
-    console.log(this.ciudad);
+  obtenerClima() {
+    this.query = false;
+    this.loading = true;
+
+    this._climaService.getClima(this.ciudad).subscribe(
+      (data) => {
+        this.loading = false;
+        this.query = true;
+        this.tempetura = data.main.temp - 273;
+        this.humedad = data.main.humidity;
+        this.clima = data.weather[0].main;
+      },
+      (error) => {
+        console.log(error);
+        this.loading = false;
+        this.error();
+      }
+    );
+  }
+
+  error() {
+    this.mostrarError = true;
+    setTimeout(() => {
+      this.mostrarError = false;
+      this.ciudad = '';
+    }, 3000);
   }
 }
--- src/app/services/clima.service.ts
+++ src/app/services/clima.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClimaService {
 
-  constructor() { }
+  url = 'https://api.openweathermap.org/data/2.5/weather?q=';
+  key = 'd20adea66199b548e70f215b1fbeb6dd';
+
+  constructor(private http: HttpClient) { }
+
+  getClima(ciudad: string): Observable<any>{
+    const URL = this.url + ciudad + '&appid='+ this.key;  
+    return this.http.get(URL)
+  }
 }
